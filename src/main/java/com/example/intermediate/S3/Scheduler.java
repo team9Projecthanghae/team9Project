1cc2df7e8fde29242c67b2935179ac44b1201d62
하지혜 <hajh5447@naver.com>
2022-08-09 11:01:22 +0900

    사진 스케쥴러 성공

--- src/main/java/com/example/intermediate/S3/Scheduler.java
+++ src/main/java/com/example/intermediate/S3/Scheduler.java
@@ -2,8 +2,11 @@ package com.example.intermediate.S3;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.example.intermediate.domain.File;
+import com.example.intermediate.domain.Post;
 import com.example.intermediate.repository.FileRepository;
+import com.example.intermediate.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +15,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 
 @PropertySource("classpath:application-s3.properties")
 @Slf4j
@@ -20,17 +24,34 @@ import java.util.List;
 public class Scheduler {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
-    private final FileRepository fileRepository;
+    private final PostRepository postRepository;
     private final AmazonS3Client amazonS3Client;
 
     // 초, 분, 시, 일, 월, 주 순서
-    @Scheduled(cron = "*/10 * * * * *")
-    public void deleteImages() throws InterruptedException {
-        ObjectListing imageList = amazonS3Client.listObjects(bucket);
-        log.info(String.valueOf(imageList.getObjectSummaries()));
-        System.out.println(imageList);
-        log.info("바보");
-        System.out.println("바보");
-
+    @Scheduled(cron = "0 */1 * * * *")
+    public void deleteImages() {
+        System.out.println("****************************************************************");
+        System.out.println("사진 정리를 시작합니다");
+        List<Post>postList =postRepository.findAll();
+        ObjectListing uploadedImageList = amazonS3Client.listObjects(bucket);
+        List<S3ObjectSummary> imageList=uploadedImageList.getObjectSummaries();
+        for(S3ObjectSummary s3ObjectSummaries:imageList){
+            int used=0;
+            for(Post post:postList){
+                if(Objects.equals(s3ObjectSummaries.getKey(), post.getImageUrl().substring(56))){
+                    used+=1;
+                    System.out.println(post.getImageUrl().substring(56));
+                    System.out.println(s3ObjectSummaries.getKey());
+                }
+            }
+            if (used<1){
+                System.out.println("***************warning************");
+                System.out.println(s3ObjectSummaries.getKey());
+                System.out.println("사진이 삭제되었습니다");
+//                amazonS3Client.deleteObject(bucket,s3ObjectSummaries.getKey());
+            }
+        }
+        System.out.println("사진 정리를 종료합니다");
+        System.out.println("****************************************************************");
     }
 }
