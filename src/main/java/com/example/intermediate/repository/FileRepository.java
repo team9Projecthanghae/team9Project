917d681a982c8a83f9df7e9771d69c416469ca7f
하지혜 <hajh5447@naver.com>
2022-08-10 10:15:00 +0900

    파일 리포지토리 누락된 코드 추가

--- src/main/java/com/example/intermediate/repository/FileRepository.java
+++ src/main/java/com/example/intermediate/repository/FileRepository.java
@@ -4,4 +4,5 @@ import com.example.intermediate.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<File,Long> {
+    void deleteByUrl(String url);
 }
